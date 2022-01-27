0797d7b328c8262c3850a87fc70b09d452f027e7
Willian <willianandrade953@gmail.com>
2022-01-27 16:28:17 -0200

    interface completed

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,6 +1,36 @@
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 
-input.addEventListener('change', () => {
-    form.submit();
+input.addEventListener('change', e => {
+    // form.submit();
+
+    const files = e.target.files;
+
+    console.log(files);
+
+    for (let i = 0; i < files.length; i++) {
+        const div = document.createElement('div');
+        div.className = 'upload-file-block';
+        document.querySelector('.main-content').append(div);
+
+        const type = document.createElement('div');
+        type.className = 'file-type';
+        div.append(type);
+        const icon = document.createElement('i');
+        icon.className = 'material-icons';
+        icon.textContent = 'image';
+        type.append(icon);
+
+        const name = document.createElement('div');
+        name.className = 'file-name';
+        name.textContent = files[i].name;
+        div.append(name);
+
+        const size = document.createElement('div');
+        size.className = 'file-size';
+        size.textContent = parseInt(files[i].size / 1024) + ' KB';
+        div.append(size);
+
+    }
+
 });
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 
-app.post('/files/upload', upload.single('teste'),(req, res) => {
-    res.send(req.body.fildname);
+app.use('/', express.static(path.join(__dirname, '..', 'public')));
+
+app.post('/files/upload', upload.single('recfile'), (req, res) => {
+    res.send({ body: req.body });
 });
 
 app.listen(3000, console.log("Listening on http://localhost:3000"));
